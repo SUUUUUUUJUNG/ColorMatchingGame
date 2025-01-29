249c16666a0b33c4ac03b76fe833c48000734d1b
SHIN SUJUNG <shinssjj79@gmail.com>
2025-01-29 14:46:18 +0900

    Initial commit

---
+++ src/main/java/com/example/colormatchinggame/ColorMatchingGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.colormatchinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorMatchingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorMatchingGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/colormatchinggame/ColorMatchingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colormatchinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorMatchingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
